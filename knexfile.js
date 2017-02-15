7073cdcc172901994c0c26eff44f1defbd30cd8b
ansonhwo <ahwong19@gmail.com>
2017-02-15 15:33:01 -0800

    Updating production knex config

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,20 @@
 exports.development = {
   client: 'postgresql',
+  connection: {
+    user: 'super',
+    database: 'subbit'
+  },
+  seeds: {
+    directory: './seeds/dev'
+  },
+  migrations: {
+    directory: './migrations',
+    tableName: 'knex_migrations'
+  }
+}
+
+exports.production = {
+  client: 'postgresql',
   connection: process.env.DATABASE_URL,
   seeds: {
     directory: './seeds/dev'
