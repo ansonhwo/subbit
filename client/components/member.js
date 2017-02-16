3ea968fea9131a2c4cb706e35d5882d7665397ad
ansonhwo <ahwong19@gmail.com>
2017-02-16 15:36:49 -0800

    Member view state resides in the Redux store rather than the DOM. #13

--- client/components/member.js
+++ client/components/member.js
@@ -5,27 +5,30 @@ const MemberView = require('./memberview.js')
 
 const { changeMemberView } = require('../actions/actions.js')
 
-const Member = ({ toggleActive }) => {
+const Member = ({ memberView, toggleActive }) => {
   return (
     <div id="member">
       <div className="ui secondary pointing two item blue menu">
-        <a className="item active" onClick={ toggleActive }>Accounts</a>
-        <a className="item" onClick={ toggleActive }>Transactions</a>
+        <a className={ "item" + (memberView === "Accounts" ? " active" : "") } onClick={ toggleActive }>Accounts</a>
+        <a className={ "item" + (memberView === "Transactions" ? " active" : "") } onClick={ toggleActive }>Transactions</a>
       </div>
       <MemberView />
     </div>
   )
 }
 
+const mapProps = state => {
+  return {
+    memberView: state.memberView
+  }
+}
+
 const mapDispatch = dispatch => {
   return {
     toggleActive: (event) => {
-      document.querySelector('#member .item.active').classList.remove('active')
-      event.target.classList.add('active')
-
       dispatch(changeMemberView(event.target.textContent))
     }
   }
 }
 
-module.exports = connect(null, mapDispatch)(Member)
+module.exports = connect(mapProps, mapDispatch)(Member)
