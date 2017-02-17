10dcf13e83497b477b733d4afd17ec899c4e8b75
ansonhwo <ahwong19@gmail.com>
2017-02-17 11:06:34 -0800

    Refactored login component. #13

--- client/components/login.js
+++ client/components/login.js
@@ -3,44 +3,38 @@ const { connect } = require('react-redux')
 const { changeUser, fetchAccounts } = require('../actions/actions.js')
 
 const Login = ({ users, username, changeUser }) => {
-  return (
-    <div id="login">
-      {
-        username !== 'User'
-          ? (
-            <div className="ui blue message">
-              <span className="content">
-                <p><i className="notched circle loading icon"></i> Logging you in...</p>
-              </span>
+  return username === 'User'
+    ? (
+      <div id="login">
+        <div className="ui fluid inverted blue segment">
+          <p className="header">Login</p>
+          <div className="ui simple selection dropdown">
+            <div className="text">{ username }</div>
+            <i className="dropdown icon"></i>
+            <div className="menu">
+              {
+                users.map((user, i) => {
+                  return <div key={ i } className="item" onClick={ changeUser }>{ user }</div>
+                })
+              }
             </div>
-          )
-          : null
-      }
-      <div className="ui fluid inverted blue segment">
-        <p className="header">Login</p>
-        {
-          username !== 'User'
-            ? (
-              <p className="welcome">{ "Welcome back, " + username }</p>
-            )
-            : (
-              <div className="ui simple selection dropdown">
-                <div className="text">{ username }</div>
-                <i className="dropdown icon"></i>
-                <div className="menu">
-                  {
-                    users.map((user, i) => {
-                      return <div key={ i } className="item" onClick={ changeUser }>{ user }</div>
-                    })
-                  }
-                </div>
-              </div>
-            )
-        }
-
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+    : (
+      <div id="login">
+        <div className="ui blue message">
+          <span className="content">
+            <p><i className="notched circle loading icon"></i> Logging you in...</p>
+          </span>
+        </div>
+        <div className="ui fluid inverted blue segment">
+          <p className="header">Login</p>
+          <p className="welcome">{ "Welcome back, " + username }</p>
+        </div>
+      </div>
+    )
 }
 
 const mapProps = state => {
